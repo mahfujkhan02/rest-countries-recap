c8fe1b59ee6501a2c7e722f79c6e99ca118648e6
mahfujkhan02 <mahfujtanim450@gmail.com>
2024-01-25 13:44:35 +0600

    added data

---
+++ js/countries.js
@@ -0,0 +1,13 @@
+ //console.log("Tanim")
+
+ const loadCountries = () =>{
+    fetch("https://restcountries.com/v3.1/all")
+    .then(res => res.json())
+    .then(data =>displayCountries(data))
+ }
+
+ const displayCountries = countries=>{
+    console.log(countries)
+ } 
+
+ loadCountries()
